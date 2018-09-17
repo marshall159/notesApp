874fb0d426457ca05fe74ddf2f3dc2a73ad9777b
Aneel Marshall <aneel.marshall@outlook.com>
2018-09-17 17:25:38 +0100

    created basic testing framework, with truthy, equality and inclusion matchers

---
+++ note-model-tests.js
@@ -0,0 +1,15 @@
+// assert.isTruthy(undefined);
+
+assert.isEqual(1 + 1, 3);
+assert.isEqual(1 + 1, 2);
+assert.isEqual(1 + 1, 3);
+assert.isEqual(1 + 1, 2);
+assert.isEqual(1 + 1, 5);
+assert.isTruthy('');
+assert.isTruthy('hi');
+assert.isIncluded([1,2,3], 5);
+assert.isIncluded([1,2,3], 2);
+assert.isIncluded('hello', 'el');
+assert.isIncluded('hello', 'zzz');
+assert.isEqual(1 + 1, 5);
+assert.showLog()
